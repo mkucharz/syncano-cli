22e11b8155a8f09495e8d64eb80c79dd79fc83a6
Maciej Kucharz <maciek@kucharz.net>
2015-12-22 14:49:22 +0100

    WIP

--- src/commands/init.js
+++ src/commands/init.js
@@ -8,98 +8,21 @@ let inquirer = require("inquirer-async");
 let mkdirp = require('mkdirp');
 let npm = require("npm");
 let denodeify = require('denodeify');
-
-let genUniqueName = require('../utils/unique-instance');
-
 let exec = require('child-process-promise').exec;
 
 // denodeify
 let writeFile = denodeify(fs.writeFile);
 let mkDir = denodeify(mkdirp);
 
-
 let prompt = inquirer.promptAsync;
 
 let settings = require('../utils/settings');
 let session = require('../utils/session');
-
+let genUniqueName = require('../utils/unique-instance');
 let templates = require('../templates');
 let helpers = require('./helpers');
 
 
-//let classesTempl = `\
-//'use strict';
-//
-//class SyncanoClass {
-//  constructor(options) {
-//    this.syncano = true;
-//  }
-//  static isSyncanoClass() {
-//    return true
-//  }
-//  static getClassName() {
-//    return this.constructor.name.toLowerCase()
-//  }
-//  set channel(name) {
-//    console.log("XXX", this.constructor.name.toLowerCase(), name)
-//  }
-//}
-//
-//
-////let SyncanoClass = require('./SyncanoClass');
-//
-//export class Book extends SyncanoClass {
-//  static schema() {
-//    return [
-//      {"name": "book_title", "type": "string"}
-//    ]
-//  }
-//}
-//`;
-
-//function installSyncano() {
-//  process.stdout.write('Installing Syncano Library... ');
-//  return exec('npm install syncano --save')
-//    .then((resp) => {
-//      console.log(format.green('Done'));
-//    })
-//    .catch((resp) => {
-//      console.log(format.red('Fail'));
-//      debug(resp);
-//    })
-//}
-
-
-//function createCBFolder() {
-//  var question = {
-//    type: "input",
-//    name: "codeboxFolder",
-//    message: "Where you want to keep your CodeBox",
-//    default: function() {
-//      return "./src/syncano/codebox";
-//    },
-//    validate: function(value) {
-//      return true;
-//    }
-//  };
-//
-//  return prompt(question)
-//    .then((answers) => {
-//      debug(`createCBFolder ${answers.codeboxFolder}`);
-//      if (answers.codeboxFolder) {
-//        process.stdout.write('Setting up CodeBox folder... ');
-//        return mkDir(answers.codeboxFolder)
-//          .catch((err) => {
-//            debug(err);
-//            console.log(format.red('Fail'))
-//          })
-//          .then(() => {
-//            settings.project.set('CodeBoxFolder', answers.codeboxFolder).save();
-//            console.log(format.green('Done'))
-//          });
-//      }
-//    });
-//}
 
 function createFilesAndFolders() {
   var question = {
@@ -148,45 +71,6 @@ function createFilesAndFolders() {
       }
     });
 }
-
-//function createClassFile(callback) {
-//  let question = {
-//    type: "input",
-//    name: "classFile",
-//    message: "Where you want to keep your Class definition file?",
-//    default: function() {
-//      return "./src/syncano/classes/book.js";
-//    },
-//    validate: function(value) {
-//      return true;
-//    }
-//  };
-//
-//  return prompt(question)
-//    .then((answers) => {
-//
-//      if (answers.classFile) {
-//        process.stdout.write('Setting up Class definition file... ');
-//        let classFile = answers.classFile;
-//
-//        return mkDir(path.dirname(answers.classFile))
-//          .catch(() => {
-//            console.log(format.red('Fail'))
-//          })
-//          .then(() => {
-//            if (fs.existsSync(classFile)) {
-//              process.stdout.write(format.yellow(`${classFile} already exists! `));
-//            } else {
-//              return writeFile(classFile, classesTempl)
-//            }
-//          })
-//          .then(() => {
-//            settings.project.set('classFile', classFile).save();
-//            console.log(format.green('Done'));
-//          });
-//      }
-//    });
-//}
 
 function generateAPIKey() {
 
@@ -244,14 +128,6 @@ module.exports = function(options) {
     console.log('Remove .syncano folder to start over.\n');
     process.exit()
   }
-
-  //buildWithWebpack()
-  //  .then((output) => {
-  //    console.log(output.compilation.errors)
-  //  })
-  //  .catch((resp) => {
-  //    console.log(resp)
-  //  });
 
   createFilesAndFolders()
     .then((resp) => {
---
+++ src/test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let Syncano = require('./syncano');
+
+console.log(Syncano);
+
+//List
+//Syncano.Book.list()
+//  .then((resp) => {
+    //console.log(resp);
+  //});
+
+//// New
+//let book = new Syncano.Book({book_title: '123'});
+//book.save()
+//  .then((resp) => {
+//    console.log(resp);
+//  });
+
+// Update
+//book.params.book_title = 'test_title';
+//book.save()
+//  .then((resp) => {
+//    console.log(resp);
+//  });
+
+
+// Delete
+//let book = new Syncano.Book({book_title: '123'});
+
+//book.save()
+//  .then((resp) => {
+//    console.log(resp);
+//  })
+//  .then((resp) => {
+//    // Delete
+//    return book.delete();
+//  })
+//  .then((resp) => {
+//    console.log(resp);
+//  });
+
+//book.on('update', () => {
+//
+//});
+
+//// New
+//let book = new Syncano.Book({book_title: '123'});
+//book.save()
+//  .then((resp) => {
+//    console.log(resp);
+//  });
